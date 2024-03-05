a379beb703387e9ed3183969f1614916acd36361
softech.series <softech.series@gmail.com>
2024-03-04 17:30:32 -0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hurricane&display=swap" rel="stylesheet">
+</head>
+
+<body class="bg-white">
+    <nav class="bg-blue-500 border-gray-200 shadow-md dark:bg-gray-900">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-md p-4 mx-auto"> 
+            <h3 class="text-xl font-bold">Gallery</h3>
+            <div class="flex gap-1">
+                <a href="login"><button class="px-5 py-1 text-white rounded-full border bg-blue-700">Login</button></a>
+                <a href="register"><button class="px-5 py-1 rounded-full border bg-slate-200">Register</button></a>
+            </div>
+    </div>
+  </nav>
+  <!--hero mobar-->
+<section>
+    <h1 class="text-center font-hurricane text-5xl "></h1>
+</section>
+<section>
+    <h1 class="text-center font-hurricane text-5xl">welcome gallery foto</h1>
+    </section>
+    </body>
+<!---gambar  ku--->
+
+<section>
+    <div class="flex justify-center gap-4">
+        <div>
+            <div class="filex flex flex-col gap-4">
+                <div class="">
+            <img src="/assets/eskrim.jpg" alt="" class="rounded-lg">
+            </div>
+            <div class="">
+            <img src="/assets/minuman.jpg" alt="">
+            </div>
+            </div>
+            </div>
+            <div class="">
+           <div><img src="/assets/gmbrpnjng.jpg" alt="" class="rounded-lg">
+        </div>
+    </div>
+</section>
+</html>
